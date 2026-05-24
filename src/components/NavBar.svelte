861969e46ea7a08b3ac85f0e8a7172db92ab73fb
Michael Jenkins <jenkinsmichpa@gmail.com>
2026-05-24 00:27:03 -0400

    mobile safari get f***ed

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -121,7 +121,7 @@
 <svelte:window onscroll={onScroll} />
 
 <nav
-  class="fixed bottom-0 left-0 right-0 z-50 md:hidden navbar-glass border-t border-surface-200-800 {dark
+  class="fixed bottom-0 left-0 right-0 z-50 md:hidden navbar-glass border-t border-surface-200-800 pb-[env(safe-area-inset-bottom,0px)] {dark
     ? 'text-white/70'
     : 'text-surface-800'}"
 >
